0e0375d6179ae9a0a03b09855d4becfd7ff3a2c8
Afthab <mohammedafthab15@gmail.com>
2023-08-17 19:35:30 +0530

    feat(database): Database connection established and pinged successfully

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+
+	"github.com/afthaab/task-manager/pkg/db"
+	"github.com/joho/godotenv"
+)
+
+func init() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalln("Error: Error in loading .env file", err)
+	}
+}
 
 func main() {
-	fmt.Println("This is my main")
+	db.ConnectToDatabase()
+	fmt.Println("Main method invoked")
 }
--- pkg/db/connection.go
+++ pkg/db/connection.go
@@ -1,1 +1,32 @@
 package db
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func ConnectToDatabase() (*mongo.Database, error) {
+	dbPassword := os.Getenv("DB_PASSWORD")
+
+	// Use the SetServerAPIOptions() method to set the Stable API version to 1
+	serverAPI := options.ServerAPI(options.ServerAPIVersion1)
+	opts := options.Client().ApplyURI(fmt.Sprintf("mongodb+srv://mongodb:%s@cluster1.kfqqheq.mongodb.net/?retryWrites=true&w=majority", dbPassword)).SetServerAPIOptions(serverAPI)
+	// Create a new client and connect to the server
+	client, err := mongo.Connect(context.TODO(), opts)
+	if err != nil {
+		panic(err)
+	}
+	ctx := context.TODO()
+	err = client.Ping(ctx, nil)
+	if err != nil {
+		return nil, fmt.Errorf("Failed to ping the MongoDB: %v", err)
+	}
+	databseConn := client.Database("taskmanager")
+
+	return databseConn, nil
+
+}
